f6a8ee494452544da5c757bac739aa2f89eb45d4
P3-R4 <90536386+P3-R4@users.noreply.github.com>
2022-08-22 19:15:58 +0200

    Update s.lua

--- s.lua
+++ s.lua
@@ -24,8 +24,3 @@ AddEventHandler('onServerResourceStart', function(resourceName)
         end
     end
 end)
-
-ESX.RegisterServerCallback('getaj', function(source,cb)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    cb(xPlayer.getIdentifier())
-end)
